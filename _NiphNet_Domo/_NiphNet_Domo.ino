649bd2c36cccf792d388a2afef5cf5f8bf3311e2
KrisP <Kris.Piessens@naturalsciences.be>
2019-01-24 15:59:09 +0100

    Double amount of sensors (8  to 16)

--- _NiphNet_Domo/_NiphNet_Domo.ino
+++ _NiphNet_Domo/_NiphNet_Domo.ino
@@ -18,10 +18,11 @@
 
       // Good tutorial on using bits: https://playground.arduino.cc/Code/BitMath#bit_pack
       // 8, including the led (blink). Also including the base counter to save memory? --> better to move to int (16 bits)? --> warning, on some arduinos an int is 32 bits!
-      byte tmrAction = 0; // B0000000 * 256 + B00000000; probably you can write 0b0000000000000000, but I'm not sure      // 16 slots to indicate no action (0) or action (1). To be tested: can bitSet() and bit() reach all 16? Note that this settin is explicit (a 0 would do). 
-      int tmrCount[7];          // Array of 8 
-      int tmrInterval[7];       // Array of 8
-      enum tgbSensor{tgbLed, tgbT01, tgbRH01, tgbP01, tgbX04, tgbX05, tgbX06, tgbX07, tgbX08, tgbX09, tgbX10, tgbX11, tgbX12, tgbX13, tgbX14, tgbX15};               // C++ command, for explicitly naming the sensors
+      
+      int tmrAction = 0; // B0000000 * 256 + B00000000; probably you can write 0b0000000000000000, but I'm not sure      // 16 slots to indicate no action (0) or action (1). To be tested: can bitSet() and bit() reach all 16? Note that this settin is explicit (a 0 would do). 
+      int tmrCount[15];          // Array of 8 
+      int tmrInterval[15];       // Array of 8
+      enum tgbSensor{tgb00_Base, tgb01_Led, tgb02_T, tgb03_RH, tgb04_P, tgb05_X, tgb06_X, tgb07_X, tgb08_X, tgb09_X, tgb10_X, tgb11_X, tgb12_X, tgb13_X, tgb14_X, tgb15_X};               // C++ command, for explicitly naming the sensors
       
       
       byte tmrBaseCnt     = 0;  // Base counter in seconds 
