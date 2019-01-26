1deaae69790f59a48245010147e26c435ee758bb
KrisP <Kris.Piessens@naturalsciences.be>
2019-01-26 21:41:21 +0100

    Debug completed for commands

--- _NiphNet_Domo/CMD_Commander.ino
+++ _NiphNet_Domo/CMD_Commander.ino
@@ -3,18 +3,21 @@
 //. Commands
 //..I'm Here
   void cmdImHere(){
+    if (set.debug) {Serial.println ('?');}                                      // Debug and code testing (results in Serial monitor)
     Serial.print("Type.Version.Serial.SensorInternal.SensorsExternal: ");
-    Serial.print(inf.tgbWhoAmI);
+    Serial.println(inf.tgbWhoAmI);
     Serial.print("Operational since: ");
     Serial.println(inf.tgbStartTGB);
     BlinkImHere();
   }
 //..Pauze
   void cmdPauze(){
+    if (set.debug) {Serial.println ('P');}                                      // Debug and code testing (results in Serial monitor)
     if (set.blinkLeds>1) {BlinkPauzeStart();}                                   // Lights on
     while (comGet() != 'P') {                                                   // Trap in this loop until P is pressed again
       delay(50);                          
     }
+    if (set.debug) {Serial.println ('P');}                                      // Debug and code testing (results in Serial monitor)
     BlinkPauzeEnd();                                                            // Lights off
   }
 //..Run (command prompt)
